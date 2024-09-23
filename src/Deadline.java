8eed1ac82a88310b640eb12f495e9ce054f2469d
MasonSimpson <158009210+MasonSimpson@users.noreply.github.com>
2024-09-23 13:11:24 -0500

    Deadline.java implemented

--- src/Deadline.java
+++ src/Deadline.java
@@ -1,3 +1,15 @@
-public class Deadline {
+import java.time.LocalDateTime;
 
+public class Deadline extends Event implements Completable{
+    public Deadline(String name, LocalDateTime deadline) {
+        setName(name);
+        setDateTime(deadline);
+    }
+    public boolean complete;
+    public void complete() {
+        complete = true;
+    }
+    public boolean isComplete() {
+        return complete;
+    }
 }
